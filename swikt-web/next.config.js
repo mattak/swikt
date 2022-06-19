a24a79a1313dc01f612ac1d2d966fdc764554ec4
mattak <mattak.me@gmail.com>
2022-06-19 23:15:28 +0900

    :wrench: support gh-pages base path prefix

--- swikt-web/next.config.js
+++ swikt-web/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = process.env.APP_ENV === 'gh-pages' ? '/swikt' : undefined;
+const assetPrefix = process.env.APP_ENV === 'gh-pages' ? '/swikt/' : undefined;
+
 const nextConfig = {
+  basePath,
+  assetPrefix,
   reactStrictMode: true,
   compress: false,
   webpack: (config, options) => {
